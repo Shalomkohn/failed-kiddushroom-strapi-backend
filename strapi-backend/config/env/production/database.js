1beb2c1ed564dc23947b4e43de2cbfa9d7977a1c
Shalomkohn2 <sbk5775sbk5775@gmail.com>
2022-12-14 17:42:10 -0500

    changed files to copy strapi docs

--- strapi-backend/config/env/production/database.js
+++ strapi-backend/config/env/production/database.js
@@ -1,20 +1,18 @@
-// path: ./config/database.js
+    // path: ./config/env/production/database.js
 
-module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-        //   shalom kohn changed this from orriginal
-            ca: env('DATABASE_CA')
+    module.exports = ({ env }) => ({
+        connection: {
+          client: 'postgres',
+          connection: {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: {
+              rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+            },
+          },
+          debug: false,
         },
-      },
-      debug: false,
-    },
-  });
+      });
